e801a2ba44132b5f3cc196b109b34ae91d06476a
Jailson Stein <steinjailson@gmail.com>
2025-04-29 11:20:38 -0300

    commit sla

--- codes/buscar_serie.py
+++ codes/buscar_serie.py
@@ -1,18 +1,18 @@
 class BuscarSerie:
-    def __init__(self, conn, nome, id):
-        self.conn = conn
+    def __init__(self, cursor, nome, id):
+        self.cursor = cursor
         self.nome = nome
         self.id = id
 
     def buscar_serie_nome(self):
-        cursor = self.conn.cursor()
+        cursor = self.cursor()
         cursor.execute("SELECT * FROM series WHERE nome = %s", (self.nome))
         series = cursor.fetchall() 
         cursor.close()
         return series
     
     def buscar_serie_id(self):
-        cursor = self.conn.cursor()
+        cursor = self.cursor()
         cursor.execute("SELECT * FROM series WHERE id = %s", (self.id))
         series = cursor.fetchall() 
         cursor.close()
